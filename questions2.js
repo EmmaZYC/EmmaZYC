38dd48af27e6de586f0d0995ac21b1a7d57c7252
EmmaZYC <73823936+EmmaZYC@users.noreply.github.com>
2020-11-26 19:04:50 +0800

    Add files via upload

---
+++ questions2.js
@@ -0,0 +1,14 @@
+var questions = [
+[ "What is my Name?", ["Emma","Ella","Enna","Eva"] ],
+[ "What is my Name?", ["Emma","Ella","Enna","Eva"] ],
+[ "What is my Name?", ["Emma","Ella","Enna","Eva"] ],
+[ "What is my Name?", ["Emma","Ella","Enna","Eva"] ],
+[ "What is my Name?", ["Emma","Ella","Enna","Eva"] ],
+[ "What is my Name?", ["Emma","Ella","Enna","Eva"] ],
+[ "What is my Name?", ["Emma","Ella","Enna","Eva"] ],
+[ "What is my Name?", ["Emma","Ella","Enna","Eva"] ],
+[ "What is my Name?", ["Emma","Ella","Enna","Eva"] ],
+[ "What is my Name?", ["Emma","Ella","Enna","Eva"] ],
+[ "What is my Name?", ["Emma","Ella","Enna","Eva"] ],
+[ "What is my Name?", ["Emma","Ella","Enna","Eva"] ],
+]
